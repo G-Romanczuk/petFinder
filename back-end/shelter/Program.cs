7afb422862dfa9b35d746ef5722de6232d5554ce
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-08-15 21:23:27 +0200

    start up config

--- back-end/shelter/Program.cs
+++ back-end/shelter/Program.cs
@@ -1,6 +1,36 @@
+
+using Microsoft.EntityFrameworkCore;
+using shelter.DataBaseContext.PetDbContext;
+using shelter.DataBaseContext.ShelterDbContext;
+using shelter.DataBaseContext.UserDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Configuration
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("UserDataConnection");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddDbContext<PetDbContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("PetDataConnection");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddDbContext<ShelterDbContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("ShelterDataConnection");
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
