53ebf77fa0d344457298736b0678c1c4d7f4b55c
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-08-21 22:43:06 +0200

    creating UserRegister api

---
+++ back-end/shelter/Controllers/RegisterController/RegisterUserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shelter.Models.UserModels;
+using shelter.UserDtos;
+
+namespace shelter.Controllers.RegisterController
+{
+    [Route("shelter/[controller]")]
+    [ApiController]
+    public class RegisterUserController : ControllerBase
+    {
+        [HttpPost(Name ="CreateUser")]
+        public IActionResult RegisterUsers([FromBody] UserRegisterDto userRegisterDto)
+        {
+            return Ok(userRegisterDto);
+        }
+    }
+}
--- back-end/shelter/UserDtos/UserRegisterDto.cs
+++ back-end/shelter/UserDtos/UserRegisterDto.cs
@@ -4,5 +4,11 @@ namespace shelter.UserDtos
 {
     public class UserRegisterDto : UserModel
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
