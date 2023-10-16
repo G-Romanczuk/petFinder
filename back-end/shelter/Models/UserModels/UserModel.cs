5301ce84e764a298d352ee020ab90f82dd5d20b1
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-10-16 20:01:40 +0200

    changing names on back instead front

--- back-end/shelter/Models/UserModels/UserModel.cs
+++ back-end/shelter/Models/UserModels/UserModel.cs
@@ -5,14 +5,14 @@ namespace shelter.Models.UserModels
     public class UserModel
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string LastName { get; set; }
+        public string Lname { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool terms { get; set; }
+        public string Phone { get; set; }
+        public bool Terms { get; set; }
         public string PostalCode { get; set; }
-        public string City { get; set; }
+        public string Town { get; set; }
         public string Address { get; set; }
         public string IncomeSource { get; set; }
 
