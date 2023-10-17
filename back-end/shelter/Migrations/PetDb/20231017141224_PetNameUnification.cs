2f15aaee2c1661bb7cc3acdb7bf0170829627a62
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2023-10-17 16:13:28 +0200

    PetModel Name Unification

---
+++ back-end/shelter/Migrations/PetDb/20231017141224_PetNameUnification.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shelter.Migrations.PetDb
+{
+    /// <inheritdoc />
+    public partial class PetNameUnification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActivityLevel",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "HasBasicCommandKnowledge",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsCatFriendly",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsChildFriendly",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsDogFriendly",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsItLikeAttention",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsNeutered",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "IsVaccinated",
+                table: "Pets");
+
+            migrationBuilder.RenameColumn(
+                name: "size",
+                table: "Pets",
+                newName: "Size");
+
+            migrationBuilder.RenameColumn(
+                name: "breed",
+                table: "Pets",
+                newName: "Breed");
+
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "Pets",
+                newName: "Age");
+
+            migrationBuilder.RenameColumn(
+                name: "sex",
+                table: "Pets",
+                newName: "Vaccination");
+
+            migrationBuilder.RenameColumn(
+                name: "Temperament",
+                table: "Pets",
+                newName: "Text");
+
+            migrationBuilder.RenameColumn(
+                name: "ShortDescription",
+                table: "Pets",
+                newName: "Temper");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Activity",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BasicTraining",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Castration",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChildFriendly",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cuddly",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Images",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherCats",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherDogs",
+                table: "Pets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activity",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "BasicTraining",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Castration",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "ChildFriendly",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Cuddly",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Images",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "OtherCats",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "OtherDogs",
+                table: "Pets");
+
+            migrationBuilder.RenameColumn(
+                name: "Size",
+                table: "Pets",
+                newName: "size");
+
+            migrationBuilder.RenameColumn(
+                name: "Breed",
+                table: "Pets",
+                newName: "breed");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Pets",
+                newName: "age");
+
+            migrationBuilder.RenameColumn(
+                name: "Vaccination",
+                table: "Pets",
+                newName: "sex");
+
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Pets",
+                newName: "Temperament");
+
+            migrationBuilder.RenameColumn(
+                name: "Temper",
+                table: "Pets",
+                newName: "ShortDescription");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ActivityLevel",
+                table: "Pets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBasicCommandKnowledge",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCatFriendly",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsChildFriendly",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDogFriendly",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsItLikeAttention",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNeutered",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVaccinated",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- back-end/shelter/Models/PetModels/PetModel.cs
+++ back-end/shelter/Models/PetModels/PetModel.cs
@@ -4,21 +4,22 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Images { get; set; }
         public string Type { get; set; }
-        public string sex { get; set; }
-        public bool IsNeutered { get; set; }
-        public string breed { get; set; }
-        public string size { get; set; }
-        public string age { get; set; }
-        public bool IsVaccinated { get; set; }
-        public bool IsChildFriendly { get; set; }
-        public bool HasBasicCommandKnowledge { get; set; }
-        public int ActivityLevel { get; set; }
-        public bool IsDogFriendly { get; set; }
-        public bool IsCatFriendly { get; set; }
-        public bool IsItLikeAttention { get; set; }
-        public string Temperament { get; set; }
-        public string ShortDescription { get; set; }
+        public string Gender { get; set; }
+        public string Castration { get; set; }
+        public string Breed { get; set; }
+        public string Size { get; set; }
+        public string Age { get; set; }
+        public string Vaccination { get; set; }
+        public string ChildFriendly { get; set; }
+        public string BasicTraining { get; set; }
+        public string Activity { get; set; }
+        public string OtherDogs { get; set; }
+        public string OtherCats { get; set; }
+        public string Cuddly { get; set; }
+        public string Temper { get; set; }
+        public string Text { get; set; }
 
     }
 }
