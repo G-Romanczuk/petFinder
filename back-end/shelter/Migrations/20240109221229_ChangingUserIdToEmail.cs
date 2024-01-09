a885472daffdd1311e2e8cafac2fef3efe8b380a
Mateusz Nosel <mateusz.nosel@microsoft.wsei.edu.pl>
2024-01-09 23:13:21 +0100

    changing userId to useremail

---
+++ back-end/shelter/Migrations/20240109221229_ChangingUserIdToEmail.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shelter.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangingUserIdToEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PetLike");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userEmail",
+                table: "PetLike",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userEmail",
+                table: "PetLike");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "PetLike",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
